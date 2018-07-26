e2cf7325affb945115f96ce925b4e3d2b103c79c
Agung <agungstwn0@gmail.com>
2018-07-26 17:01:43 +0700

    make connection Bluetoth device

--- thermalprintlib/src/main/java/id/agung/android/thermalprintlib/connection/ThermalBluetooth.java
+++ thermalprintlib/src/main/java/id/agung/android/thermalprintlib/connection/ThermalBluetooth.java
@@ -1,7 +1,12 @@
 package id.agung.android.thermalprintlib.connection;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by agung on 26/07/18.
@@ -25,5 +30,21 @@ public class ThermalBluetooth {
         }
     }
 
-
+    public List<BluetoothDevice> getPairedDevice(){
+        List<BluetoothDevice> mBluetoothDeviceList = new ArrayList<>();
+        if (isBluetoothEnabled()){
+            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+            if (pairedDevices.size() > 0){
+                for (BluetoothDevice device : pairedDevices){
+                    mBluetoothDeviceList.add(device);
+                }
+                return mBluetoothDeviceList;
+            }else {
+                Log.i(TAG, "No Paired Device ");
+            }
+        }else {
+            Log.i(TAG, "Bluetooth is Disabled");
+        }
+        return mBluetoothDeviceList;
+    }
 }
