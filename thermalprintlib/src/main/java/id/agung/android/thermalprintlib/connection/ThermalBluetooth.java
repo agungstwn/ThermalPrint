55b40eca6281b87e7892c5972560488e6912c29a
Agung <agungstwn0@gmail.com>
2018-07-26 15:37:01 +0700

    add bluetooth permission

--- thermalprintlib/src/main/java/id/agung/android/thermalprintlib/connection/ThermalBluetooth.java
+++ thermalprintlib/src/main/java/id/agung/android/thermalprintlib/connection/ThermalBluetooth.java
@@ -1,9 +1,29 @@
 package id.agung.android.thermalprintlib.connection;
 
+import android.bluetooth.BluetoothAdapter;
+import android.util.Log;
+
 /**
  * Created by agung on 26/07/18.
  */
 
 public class ThermalBluetooth {
+    private final String TAG = getClass().getSimpleName();
+
+    private BluetoothAdapter mBluetoothAdapter;
+
+    public ThermalBluetooth(BluetoothAdapter bluetoothAdapter) {
+        this.mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    public boolean isBluetoothEnabled(){
+        if (mBluetoothAdapter.isEnabled()){
+            return true;
+        }else {
+            Log.i(TAG, "isBluetoothEnabled: ");
+            return false;
+        }
+    }
+
 
 }
